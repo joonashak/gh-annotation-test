9a86dae413cc68ec961661b607fca376bc8eda29
joonashak <joonashak@proton.me>
2023-09-29 12:22:22 +0300

    Annotation output from script

--- annotate.sh
+++ annotate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GREP=$(grep -R "// TODO:")
+GREP=$(grep -Rn "// TODO:")
 
-while IFS=':' read -r path message; do
-  echo "${path} >>> ${message}"
+while IFS=':' read -r path line message; do
+  echo "::warning file=${path},line=${line}::${message}"
 done <<< "$GREP"
